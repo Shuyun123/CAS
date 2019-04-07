4541a1d2a5591ffe59394c598d2d986646e3c523
anumbrella <shuyun7620@fiberhome.com>
2019-04-07 10:46:39 +0800

    [A] add file

---
+++ Chapter9/client-demo/src/main/java/net/anumbrella/sso/entity/User.java
@@ -0,0 +1,64 @@
+
+package net.anumbrella.sso.entity;
+
+import javax.persistence.*;
+
+/**
+ * @author Anumbrella
+ */
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [username=" + username + ", password=" + password + "]";
+    }
+
+}
