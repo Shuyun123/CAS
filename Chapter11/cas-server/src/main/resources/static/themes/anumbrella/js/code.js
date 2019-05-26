dd7a08d4d6d03f87dee8fc57434b7983ce3ab654
anumbrella <1197173684@qq.com>
2019-05-26 19:14:12 +0800

    add demo

---
+++ Chapter11/cas-server/src/main/resources/static/themes/anumbrella/js/code.js
@@ -0,0 +1,29 @@
+function changeCode(){
+    var node = document.getElementById("captcha_img");
+    var url = node.src;
+    // 修改验证码
+    if (node){
+        if(url.indexOf("?") >= 0){
+            node.src = url.split('?')[0] +'?id='+uuid();
+        }else{
+            node.src = url +'?id='+uuid();
+        }
+
+    }
+}
+
+
+function uuid(){
+    //获取系统当前的时间
+    var d = new Date().getTime();
+    //替换uuid里面的x和y
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        //取余 16进制
+        var r = (d + Math.random()*16)%16 | 0;
+        //向下去整
+        d = Math.floor(d/16);
+        //toString 表示编程16进制的数据
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
